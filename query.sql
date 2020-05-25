a7b8fcfd1ce302291a81c96ac00abe10596efc2f
SCURRYYY <60529752+SCURRYYY@users.noreply.github.com>
2020-05-25 19:54:45 -0400

    Update query.sql

--- query.sql
+++ query.sql
@@ -3,6 +3,6 @@ FROM (SELECT name, ts , high, substring(ts,12,2) AS hour
       FROM "finance-db") a1 ,
 (SELECT name, max(high) as max_high, substring(ts,12,2) AS hour
    FROM "finance-db"
-   GROUP BY name,  substring(ts,12,2))a2
+   GROUP BY name,  substring(ts,12,2)) a2
 WHERE a1.name = a2.name  AND a1.hour = a2.hour AND a1.high = a2.max_high
 ORDER BY a1.name, a2.hour;
