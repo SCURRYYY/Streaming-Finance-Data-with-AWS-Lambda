cc180ebf67813a4357f1e87c29c694dbdb0da76d
SCURRYYY <60529752+SCURRYYY@users.noreply.github.com>
2020-05-24 22:53:58 -0400

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,4 +1,4 @@
-select name, substr(ts, 12, 2) as hour, max(high) as max_high
+select name, ts, substr(ts, 12, 2) as hour, max(high) as max_high
 from athena
-group by name, substr(ts, 12, 2)
+group by name,ts,  substr(ts, 12, 2)
 order by hour DESC;
