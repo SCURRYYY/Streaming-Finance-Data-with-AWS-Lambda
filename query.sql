4e9732e3299949e0a070e8630c580cf630e3276b
SCURRYYY <60529752+SCURRYYY@users.noreply.github.com>
2020-05-25 10:14:01 -0400

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,4 +1,8 @@
-select name, ts, substr(ts, 12, 2) as hour, max(high) as max_high
-from athena
-group by name,ts,  substr(ts, 12, 2)
-order by hour DESC;
+SELECT a1.name,a1.ts,a1.hour,a2.max_high
+FROM (SELECT name, ts , high, substring(ts,12,2) AS hour
+      FROM "finance-db") a1 ,
+(SELECT name, max(high) as max_high, substring(ts,12,2) AS hour
+   FROM "finance-db"
+   GROUP BY name,  substring(ts,12,2))a2
+WHERE a1.name = a2.name  AND a1.hour = a2.hour AND a1.high = a2.max_high
+ORDER BY a1.name, a2.hour;
